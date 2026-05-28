aadb9389d5669478414bab7e640f88507a311656
Justin Dysarz <justindysarz@tuta.io>
2026-05-28 18:10:20 -0500

    Update installer

--- install.sh
+++ install.sh
@@ -1,3 +1,2 @@
 #!/bin/sh
-
-cmake CMakeLists.txt && make -j$(($(nproc) + 2)) && sudo make install || doas make install
+cmake CMakeLists.txt && make -j$(($(nproc) + 2)) && sudo make install  && sudo setcap cap_net_raw,cap_net_admin=ep $(which dumpcap)
